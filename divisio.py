ca2e9ae5dc9190c0237e2c08c32370eff51725c3
nayeriam <nanglada@instituticaria.cat>
2024-10-18 10:37:06 +0200

    Add files via upload

--- divisio.py
+++ divisio.py
@@ -1,13 +1,8 @@
-dividend = int(input("Introdueix el dividend (nombre enter): "))
-divisor = int(input("Introdueix el divisor (nombre enter): "))
+dividend = int(input("Introdueix el dividend: "))
+divisor = int(input("Introdueix el divisor: "))
+quocient = dividend // divisor
+residu = dividend % divisor
+print(f"divisió: {dividend}/{divisor}")
+print(f"quocient: {quocient}")
+print(f"residu: {residu}")
 
-if divisor != 0:
-    divisio = dividend/divisor
-    quocient = dividend // divisor
-    residu = dividend % divisor
-
-    print(f"divisió : {dividend}/{divisor}")
-    print(f"quocient : {quocient}")
-    print(f"residu : {residu}")
-else:
-    print("Error: El divisor no pot ser zero.")
