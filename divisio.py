909bcc9e604816d2ef21bacf3c52512246dbd4c2
nayeriam <nanglada@instituticaria.cat>
2024-10-18 11:03:16 +0200

    Delete divisio.py

--- divisio.py
+++
@@ -1,7 +0,0 @@
-Divident = int(input("Introdueix el Divident:"))
-Divisor = int(input("Introdueix el Divisor:"))
-Quocient = Divident // Divisor
-Residu = Divident % Divisor
-print(f"Divisió: {Divident}/{Divisor}")
-print(f"Quocient: {Quocient}")
-print(f"Residu: {Residu}")
