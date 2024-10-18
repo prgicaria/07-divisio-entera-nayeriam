3630173c2e1d21198fd218b9442d9905d72fef84
nayeriam <nanglada@instituticaria.cat>
2024-10-18 11:12:36 +0200

    Add files via upload

--- divisio.py
+++ divisio.py
@@ -4,4 +4,4 @@ Quocient = Divident // Divisor
 Residu = Divident % Divisor
 print(f"Divisió: {Divident}/{Divisor}")
 print(f"Quocient: {Quocient}")
-print(f"Residu: {Residu}")
+print(f"Residu: {Residu}")
